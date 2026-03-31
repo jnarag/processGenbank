d08e62f4321b493b1719912f90d9b77943a2cc1d
jnarag <jna.raghwani@gmail.com>
2026-03-31 19:07:41 +0100

    added versions of process_gb and process_gb_seg that also extracts sequences

--- genbank_utils/genbank_to_metadata.py
+++ genbank_utils/genbank_to_metadata.py
@@ -1,5 +1,7 @@
 __author__ = 'jayna'
 
+import Bio.SeqRecord
+from Bio import SeqRecord
 from Bio import SeqIO
 from Bio import AlignIO
 from datetime import datetime, date
@@ -165,7 +167,6 @@ def process_gb(gb_file, working_dir, virus_family):
     input_handle.close()
     out_tablefile_handle.close()
 
-
 def process_gb_seg(gb_file, working_dir, virus_family):
 
     ### GENBANK FILE ###
@@ -317,6 +318,510 @@ def process_gb_seg(gb_file, working_dir, virus_family):
 
         out_tablefile_handle.flush()
 
+
+    # close all the files once finished processing the genbank file.
+
+    input_handle.close()
+    out_tablefile_handle.close()
+
+def process_gb_to_fasta(gb_file, working_dir, virus_family, min_length, host_taxafile):
+
+    ### GENBANK FILE ###
+    # create a variable to read your genbank file (i.e. gb_file). Note "r", which indicates the file is readable.
+    input_handle = open(gb_file, "r")
+    count = 0
+
+    ### METADATA FILE ###
+    # out_tablefile is a string variable that corresponds to the filename of the metadata file. Here I am using str_replace so that the filename is similar to the output fasta filena,e
+
+    out_tablefile = working_dir + "/" + virus_family + "_metadata.csv"
+
+    try:
+        os.makedirs(working_dir + "/" + virus_family)
+
+    except FileExistsError:
+        # directory already exists
+        pass
+
+    # Similar to output_handle, create a variable so that you can write to the metadata file.
+    out_tablefile_handle = open(out_tablefile, "w")
+
+    # Write out the column names to the metadata file.
+    out_tablefile_handle.write(
+        "Sequence Name,Accession_no,Strain,Organism,Species/Genus,Host,Country,Collection_date,Year,Sequence_length,%Sequence_gap,Pubmed_id\n")
+
+    with open(host_taxafile) as f:
+        rodentia_taxa = f.read().splitlines()
+
+    ### PROCESS GENBANK FILE ###
+
+    species_dict = {}
+
+    for index, record in enumerate(SeqIO.parse(input_handle, "genbank")):
+
+        # extract genus of virus sequence
+        taxonomy = record.annotations["taxonomy"]
+
+        if(virus_family == "Coronaviridae" and len(taxonomy) > 9):
+
+            species = taxonomy[8] + ":" + taxonomy[9]
+
+        elif(virus_family == "Phenuiviridae" and len(taxonomy) > 8):
+
+            species = taxonomy[7] + ":" + taxonomy[8]
+            print(species)
+
+        elif(virus_family == "Paramyxoviridae" and len(taxonomy) > 9):
+            species = taxonomy[8] + ":" + taxonomy[9]
+
+        elif (virus_family == "Paramyxoviridae" and len(taxonomy) > 8):
+            species = (taxonomy[7] +
+                       ":" + taxonomy[8])
+
+        elif (virus_family == "Paramyxoviridae" and len(taxonomy) < 8):
+            species = taxonomy[6] + ":" + taxonomy[7]
+
+        elif(virus_family == "Rhabdoviridae" and len(taxonomy) > 9):
+
+            species = taxonomy[8] + ":" + taxonomy[9]
+
+        elif (len(taxonomy) >= 9):
+
+            species = taxonomy[7] + ":" + taxonomy[8]
+
+        else:
+            species = taxonomy[len(taxonomy) - 2] + ":" + taxonomy[len(taxonomy) - 1]
+            print(species)
+
+
+        # For each record in the genbank file, extracts the different subfields (or qualifiers) in the FEATURES field
+        features_qualifiers = record.features[0].qualifiers
+
+        # strain_name = record.annotations['accessions'][0]
+
+        # Metadata / fields we are interested in extracting per sequence
+        d = ""
+        name = ""
+        year = ""
+        host = "NA"
+        isolate = "NA"
+        country = "NA"
+        t_date = "NA"
+        accession_no = record.annotations['accessions'][0]
+        organism = features_qualifiers['organism'][0]
+        flag = ""
+
+        # Get the Pubmed id of the publication associated with the record/sequence
+        reference = record.annotations['references'][0]
+        pubmed_id = reference.pubmed_id
+
+        # Extracting which host the sequence was obtained from. This could be stored in /host= field or /isolation source
+        if 'host' in features_qualifiers.keys():
+
+            host = features_qualifiers['host'][0]
+
+            host = str(host).replace(' ', ':')
+
+
+            # [optional] print out host to screen to see how the above line changes the output
+
+        elif 'isolation_source' in features_qualifiers.keys():
+            host = features_qualifiers['isolation_source'][0]
+
+            host = str(host).replace(' ', ':')
+
+        # Extract the country information of where the sequence was sampled from
+        if 'geo_loc_name' in features_qualifiers.keys():
+            country = features_qualifiers['geo_loc_name'][0]
+            country_parts = country.split(':')
+            country = country_parts[0]
+            country = country.replace(" ", ":")
+
+        # Extract the date of collection of the sequence
+        if 'collection_date' in features_qualifiers.keys():
+            d = features_qualifiers['collection_date'][0]
+
+        # Extract isolate name
+        if 'strain' in features_qualifiers.keys():
+            isolate = features_qualifiers['strain'][0]
+
+        if 'note' in features_qualifiers.keys():
+            notes = features_qualifiers['note'][0]
+
+            if 'experiment' in notes or 'vaccine' in notes:
+                flag = "experiment or vaccine"
+
+        if 'lab_host' in features_qualifiers.keys():
+            flag = "lab host"
+
+        if 'cell culture' in host:
+            flag = "cell culture"
+
+        # The Date field may need further processing.
+        t_date, year = process_date(d)
+
+        name = accession_no + "_" + country + "_" + host + "|" + str(t_date) + "|" + str.replace(species, " ",
+                                                                                                 "..") + "|" + str.replace(
+            organism, " ", ":")
+
+        count += 1
+
+        tmp_min_length = min_length
+
+        for f in record.features:
+
+            if (f.type == "CDS"):
+
+                seq = f.location.extract(record).seq
+
+                if (len(str(seq)) > tmp_min_length):
+                    tmp_min_length = len(str(seq))
+                    record.seq = seq
+
+        N_percentage = (str(record.seq).count('N') / len(str(record.seq)))*100
+
+        new_record = Bio.SeqRecord.SeqRecord(name=name, seq=record.seq)
+
+        if (species not in species_dict.keys()):
+            species_dict[species] = []
+            species_dict[species].append(new_record)
+
+            # This line writes out all the metadata collected for each record to the metadata output file (out_tablefile)
+            out_tablefile_handle.write(
+                name + "," +
+                accession_no + "," +
+                isolate + "," +
+                organism + "," +
+                species + "," +
+                host + "," +
+                country.replace(":", " ") + "," +
+                str(t_date) + "," +
+                str(year) + "," +
+                str(len(record.seq)) + "," +
+                "{:.2f}".format(N_percentage) + "," +
+                str(pubmed_id) + "," +
+                flag + "\n")
+
+            out_tablefile_handle.flush()
+
+        else:
+            species_dict[species].append(new_record)
+
+            # This line writes out all the metadata collected for each record to the metadata output file (out_tablefile)
+            out_tablefile_handle.write(
+                name + "," +
+                accession_no + "," +
+                isolate + "," +
+                organism + "," +
+                species + "," +
+                host.replace(":", " ") + "," +
+                country.replace(":", " ") + "," +
+                str(t_date) + "," +
+                str(year) + "," +
+                str(len(record.seq)) + "," +
+                "{:.2f}".format(N_percentage) + "," +
+                str(pubmed_id) + "," +
+                flag + "\n")
+
+            out_tablefile_handle.flush()
+
+    # Write out sequences to file
+    try:
+        os.makedirs(working_dir + "/" + virus_family)
+
+    except FileExistsError:
+        # directory already exists
+        pass
+
+    for s in species_dict.keys():
+
+        species_name = (s.replace(" ", "_"))
+
+        ### OUTPUT FASTA FILE ###
+        # create a variable to write data to your output file (i.e. out_fastafile). Note "w", which indicates the file is writable.
+
+        fastoutfile = working_dir + "/" + virus_family + "/" + str(species_name) + ".fasta"
+
+        out_fastoutfile_handle = open(fastoutfile, "w")
+
+        seq_records = species_dict[s]
+
+        for record in seq_records:
+            out_fastoutfile_handle.write(">" + str(record.name) + "\n")
+            out_fastoutfile_handle.write(str(record.seq) + "\n")
+
+        out_fastoutfile_handle.close()
+
+    # close all the files once finished processing the genbank file.
+
+    input_handle.close()
+    out_tablefile_handle.close()
+
+def process_gb_to_fasta_seg(gb_file, working_dir, virus_family, segment_length, segments, host_taxafile):
+    product_gene_list = []
+    ### GENBANK FILE ###
+    # create a variable to read your genbank file (i.e. gb_file). Note "r", which indicates the file is readable.
+    input_handle = open(gb_file, "r")
+    count = 0
+
+    ### METADATA FILE ###
+    # out_tablefile is a string variable that corresponds to the filename of the metadata file. Here I am using str_replace so that the filename is similar to the output fasta filena,e
+    out_tablefile = working_dir + "/" + virus_family + "_metadata.csv"
+
+    try:
+        os.makedirs(working_dir + "/" + virus_family)
+
+    except FileExistsError:
+        # directory already exists
+        pass
+
+    # Similar to output_handle, create a variable so that you can write to the metadata file.
+    out_tablefile_handle = open(out_tablefile, "w")
+
+    # Write out the column names to the metadata file.
+    out_tablefile_handle.write(
+        "Sequence Name,Accession_no,Segment,Strain,Organism,Species/Genus,Host,Country,Collection_date,Year,Sequence_length,Pubmed_id,Flag\n")
+
+    with open(host_taxafile) as f:
+        rodentia_taxa = f.read().splitlines()
+
+    ### PROCESS GENBANK FILE ###
+
+    species_dict_by_seg = {}
+
+    for seg in segments.keys():
+        species_dict_by_seg[seg] = {}
+
+    for index, record in enumerate(SeqIO.parse(input_handle, "genbank")):
+
+        # extract genus of virus sequence
+        taxonomy = record.annotations["taxonomy"]
+
+
+        # print(len(taxonomy), taxonomy)
+
+        species_or_genus = "NA"
+        taxonomy_length = len(taxonomy)
+        if taxonomy_length == 8:
+            species_or_genus = "unclassified " + taxonomy[7]
+
+        elif(virus_family == "Phenuiviridae" and len(taxonomy) > 8):
+            species_or_genus = taxonomy[7] + ":" + taxonomy[8]
+
+        elif taxonomy_length == 9:
+
+            if "Mammarenavirus" in record.features[0].qualifiers['organism'][0]:
+                species_or_genus = record.features[0].qualifiers['organism'][0]
+            else:
+                species_or_genus = taxonomy[8]
+
+        elif taxonomy_length == 10:
+
+            if "Orthohantavirus" in record.features[0].qualifiers['organism'][0]:
+                species_or_genus = record.features[0].qualifiers['organism'][0]
+            else:
+                species_or_genus = taxonomy[9]
+
+        elif taxonomy_length == 11:
+            species_or_genus = taxonomy[10]
+
+        # For each record in the genbank file, extracts the different subfields (or qualifiers) in the FEATURES field
+        features_qualifiers = record.features[0].qualifiers
+
+        # strain_name = record.annotations['accessions'][0]
+
+        # Metadata / fields we are interested in extracting per sequence
+        d = ""
+        name = ""
+        year = ""
+        host = "NA"
+        isolate = "NA"
+        country = "NA"
+        t_date = "NA"
+        accession_no = record.annotations['accessions'][0]
+        organism = features_qualifiers['organism'][0].replace(" ", "..")
+        flag = ""
+
+        # Get the Pubmed id of the publication associated with the record/sequence
+        reference = record.annotations['references'][0]
+        pubmed_id = reference.pubmed_id
+
+        # Extracting which host the sequence was obtained from. This could be stored in /host= field or /isolation source
+        if 'host' in features_qualifiers.keys():
+
+            host = features_qualifiers['host'][0]
+
+            host = str(host).replace(' ', ':')
+
+            # [optional] print out host to screen to see how the above line changes the output
+
+        elif 'isolation_source' in features_qualifiers.keys():
+            host = features_qualifiers['isolation_source'][0]
+
+            host = str(host).replace(' ', ':')
+
+        # Extract the country information of where the sequence was sampled from
+        if 'geo_loc_name' in features_qualifiers.keys():
+            country = features_qualifiers['geo_loc_name'][0]
+            country_parts = country.split(':')
+            country = country_parts[0]
+            country = country.replace(" ", ":")
+
+        # Extract the date of collection of the sequence
+        if 'collection_date' in features_qualifiers.keys():
+            d = features_qualifiers['collection_date'][0]
+
+        # Extract isolate name
+        if 'strain' in features_qualifiers.keys():
+            isolate = features_qualifiers['strain'][0]
+
+        if 'note' in features_qualifiers.keys():
+            notes = features_qualifiers['note'][0]
+
+            if 'experiment' in notes or 'vaccine' in notes:
+                flag = "experiment or vaccine"
+
+        if 'lab_host' in features_qualifiers.keys():
+            flag = "lab host"
+
+        if 'cell culture' in host:
+            flag = "cell culture"
+
+        # The Date field may need further processing.
+        t_date, year = process_date(d)
+
+        # species_or_genus = get_virus_taxonomic_rank(record, taxonomic_level)
+
+        name = accession_no + "_" + country + "_" + host + "|" + str(t_date) + "|" + str.replace(species_or_genus, " ",
+                                                                                                 "..") + "|" + str.replace(
+            organism, " ", ":")
+
+        count += 1
+
+        segment_records = {}
+        for seg in segments.keys():
+            segment_records[seg] = None
+
+
+        for f in record.features:
+            if f.type == "CDS":
+                # print(index, f.qualifiers.keys())
+
+                product_or_gene = "NULL"
+                if ("product" in f.qualifiers.keys()):
+
+                    product_or_gene = f.qualifiers["product"][0]
+                    product_gene_list.append(f.qualifiers["product"][0])
+
+                elif ("gene" in f.qualifiers.keys()):
+                    product_or_gene = f.qualifiers["gene"][0]
+                    product_gene_list.append(f.qualifiers["gene"][0])
+
+                if (product_or_gene in ["hypothetical protein", "polyprotein"]):
+
+                    if ("segment" in record.features[0].qualifiers.keys()):
+                        product_or_gene = record.features[0].qualifiers["segment"][0] + " protein"
+
+                for seg in segments.keys():
+
+                    tmp_min_length = segment_length[seg]
+
+                    if product_or_gene in segments[seg]:
+
+                        seq = f.location.extract(record).seq
+
+                        if (len(str(seq)) > tmp_min_length):
+                            tmp_min_length = len(str(seq))
+
+                            N_percentage = str(seq).count('N') / len(str(seq))
+
+                            if (N_percentage >= 0.1):
+                                continue
+
+                            else:
+
+                                segment_records[seg] = Bio.SeqRecord.SeqRecord(name=name, seq=seq)
+
+                        else:
+                            continue
+
+        #     new_record = Bio.SeqRecord.SeqRecord(name=name, seq=record.seq)
+        #
+
+        for seg in segments.keys():
+
+            seg_record = segment_records[seg]
+
+            if (seg_record is not None):
+
+                if (species_or_genus not in species_dict_by_seg[seg].keys()):
+
+                    species_dict_by_seg[seg][species_or_genus] = []
+                    species_dict_by_seg[seg][species_or_genus].append(seg_record)
+
+                    # This line writes out all the metadata collected for each record to the metadata output file (out_tablefile)
+                    out_tablefile_handle.write(
+                        name + "," +
+                        accession_no + "," +
+                        seg + "," +
+                        isolate + "," +
+                        organism + "," +
+                        species_or_genus + "," +
+                        host + "," +
+                        country.replace(":", " ") + "," +
+                        str(t_date) + "," +
+                        str(year) + "," +
+                        str(len(seg_record.seq)) + "," +
+                        str(pubmed_id) + "," +
+                        flag + "\n")
+
+                    out_tablefile_handle.flush()
+
+                else:
+                    species_dict_by_seg[seg][species_or_genus].append(seg_record)
+
+                    # This line writes out all the metadata collected for each record to the metadata output file (out_tablefile)
+                    out_tablefile_handle.write(
+                        name + "," +
+                        accession_no + "," +
+                        seg + "," +
+                        isolate + "," +
+                        organism + "," +
+                        species_or_genus + "," +
+                        host.replace(":", " ") + "," +
+                        country.replace(":", " ") + "," +
+                        str(t_date) + "," +
+                        str(year) + "," +
+                        str(len(seg_record.seq)) + "," +
+                        str(pubmed_id) + "," +
+                        flag + "\n")
+
+                    out_tablefile_handle.flush()
+
+
+    for seg in segments.keys():
+        try:
+            os.makedirs(working_dir + "/" + virus_family + "/" + seg)
+
+        except FileExistsError:
+            # directory already exists
+            pass
+
+        for s in species_dict_by_seg[seg].keys():
+
+            species_name = (s.replace(" ", "_"))
+
+            fastoutfile = working_dir + "/" + virus_family + "/" + seg + "/" + str(species_name) + ".fasta"
+
+            out_fastoutfile_handle = open(fastoutfile, "w")
+
+            seq_records = species_dict_by_seg[seg][s]
+
+            for record in seq_records:
+                out_fastoutfile_handle.write(">" + str(record.name) + "\n")
+                out_fastoutfile_handle.write(str(record.seq) + "\n")
+
+            out_fastoutfile_handle.close()
 
     # close all the files once finished processing the genbank file.
 
